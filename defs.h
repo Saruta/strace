1f1a7e0d8ff63c19505e9d97c6e5fd22ed84cd0b
Fabien Siron <fabien.siron@epita.fr>
2016-08-14 12:44:09 +0000

    Introduce a dummy netlink attributes parser
    Since netlink heavily use its attributes, they have to be decoded. Here
    is the first step to decode it, detect all the attributes and print
    its structure (len and decoded type). Attribute's data will be decoded
    later.
    
    * netlink.c (fetch_nlattr, decode_nlattr): New function.
    * defs.h (decode_nlattr): Add prototype.
    * netlink_sock_diag (decode_inet_diag_msg, decode_netlink_diag_msg,
    decode_packet_diag_msg, decode_unix_diag_msg): Use decode_nlatttr().
    * xlat/netlink_attr_idiag.in: New file.
    * xlat/netlink_attr_udiag.in: Likewise.
    * xlat/netlink_attr_pdiag.in: Likewise.
    * xlat/netlink_attr_ndiag.in: Likewise.

--- defs.h
+++ defs.h
@@ -689,6 +689,7 @@ extern void tprint_iov_upto(struct tcb *, unsigned long, unsigned long,
 extern void decode_netlink(struct tcb *, int, unsigned long, unsigned long);
 extern void decode_netlink_sock_diag(struct tcb *, unsigned long, unsigned long,
 				     unsigned int, int);
+extern void decode_nlattr(struct tcb *, unsigned long ,unsigned long, struct xlat*, const char *);
 extern void tprint_open_modes(unsigned int);
 extern const char *sprint_open_modes(unsigned int);
 extern void print_seccomp_filter(struct tcb *, unsigned long);
